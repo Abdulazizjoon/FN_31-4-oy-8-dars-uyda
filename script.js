65543f2a6a382ec6448305c345178b3f4821cff6
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-11-11 14:00:37 +0500

    .

---
+++ script.js
@@ -0,0 +1,96 @@
+//pop uchun masala
+//1-masala
+// let arr = [10, 20, 30, 40, 50];
+// function puh(a) {
+//   let sum = 0;
+//   for (let j = 0; j < a.length; j++) {
+//     sum += a[j];
+//   }
+//   for (let i = 0; sum > 100; i++) {
+//     sum -= a.pop();
+//   }
+//   return sum;
+// }
+// let res = puh(arr);
+// console.log(res);
+
+//2-masala
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// function pop(a) {
+//     for (let i = 0; i <3; i++) {
+//         a.pop()
+//     }
+//     return a
+// }
+// let res=pop(arr)
+// console.log(res);
+
+//3-masala
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// function pop(a) {
+//     for (let i = 0; i <1; i++) {
+//         a.pop()
+//     }
+//     for (let j = a.length; j >=0; j--) {
+//         console.log(a[j]);
+//     }
+// }
+// let res = pop(arr)
+
+//shift
+//1-masala
+// let arr = ["Ali", "Abdulaziz", "Bobur", "Aziz",'Aziza'];
+// function strShift(a) {
+//   let sum = a;
+//   for (let i = 0; a.length > 3; i++) {
+//       sum.shift()
+//   }
+//   return sum;
+// }
+// let res = strShift(arr);
+// console.log(res);
+
+//2-masala
+// let arr = [100, 200, 300, 400];
+// function strShift(a) {
+//   let sum = 0;
+//   for (let i = 1; i < a.length; i++) {
+//       sum = a.splice(i,1);
+//   }
+//   return sum;
+// }
+// let res = strShift(arr);
+// console.log(res);
+
+//3-masala
+// let arr = [2, 4, 6, 8];
+// function firstremove(a) {
+//         a.shift();
+//     for (let i = 0; i < a.length; i++) {
+//         a[i] = a[i] * 2
+//     }
+
+// }
+// firstremove(arr)
+// console.log(arr);
+
+//unshift
+//1-masala
+// let arr = [10, 20];
+// function firsadd(a) {
+//   let sum = 0;
+//   for (let j = 0; j < a.length; j++) {
+//     sum += a[j];
+//   }
+//   for (let i = 0; sum < 50; i++) {
+//       a.unshift(30);
+//       sum+=30
+//   }
+//   console.log(a);
+//   return sum;
+// }
+// firsadd(arr);
+
+
+//2-masala
+
